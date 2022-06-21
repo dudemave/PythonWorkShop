08faba854fd0226629256d90d5ca92a77e889ad3
dudemave <marekheinrich@web.de>
2022-06-21 15:39:52 +0200

    fixing indentation

--- unittest_machine.py
+++ unittest_machine.py
@@ -40,19 +40,19 @@ class TestMachineMethods(unittest.TestCase):
         self.assertEqual(5100000, machine_one.total_register)
 
 
-def test_frank(self):
+    def test_frank(self):
 
-    machine_one = franco_machine.machine.machine(
-        machine_type="PostBaseMini",
-        country = 'de',
-        sw_version = '0.0.1'
-    )
+        machine_one = franco_machine.machine.machine(
+            machine_type="PostBaseMini",
+            country = 'de',
+            sw_version = '0.0.1'
+            )
 
-    machine_one.load_money_amount(100000)
-    machine_one.frank(2500)
+        machine_one.load_money_amount(100000)
+        machine_one.frank(2500)
 
-    self.assertEqual(97500, machine_one.haben)
-    self.assertEqual(10000, machine_one.total_register)
+        self.assertEqual( 97500, machine_one.haben)
+        self.assertEqual(100000, machine_one.total_register)
 
 
 
