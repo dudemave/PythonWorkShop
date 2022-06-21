68e5d32822620431969636acafd80cc2bb80791f
123wall <123wallroth321@posteo.de>
2022-06-21 11:03:32 +0200

    myfirstexercise

--- exercise_day_2_andreas.py
+++ exercise_day_2_andreas.py
@@ -1,1 +1,10 @@
-# 
+#!/usr/bin/env python3
+
+myVarName = 'Karl'
+myVarDate = '2022-02-18'
+myVarNumber = 1234556678
+myVarOtherIndexNumber = 1212
+
+myList = [myVarName, myVarDate, myVarNumber, myVarOtherIndexNumber]
+
+print (myList)
