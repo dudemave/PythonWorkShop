22e1b9aa5b7bb204870f871435dff2e220462000
dudemave <marekheinrich@web.de>
2022-06-21 12:11:25 +0200

    some workshop wip

--- exercise_day_2_andreas.py
+++ exercise_day_2_andreas.py
@@ -9,10 +9,45 @@ myVarOtherIndexNumber = 1212
 
 myList = [myVarName, myVarDate, myVarNumber, myVarOtherIndexNumber]
 
+
+class theNextClass():
+    #double underscore = dunder
+
+    def __init__(self, name_string):
+        self.name = name_string
+
+        self.say_my_name()
+
+
+    def say_my_name(self):
+        print(self.name)
+
+
+def myFunction():
+    """ this is a very cool function """
+
+
+    for i in myList :
+        print (i)
+
+    print("Hallo")
+
+
+##### put the object here
+#theNextClass()
+newObj = theNextClass('xxx')
+secondObj = theNextClass('blub')
+
+secondObj.say_my_name()
+
+secondObj.my_second_name = 'bla'
+print (secondObj.my_second_name)
+
+
 print(myVarDate.year)
 
 print (myList)
 
 
-for i in myList :
-    print (i)
+
+myFunction()
