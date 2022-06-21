7de7e5d3ef031936e3ecf16f98b450f16e200688
Axel Schwietz <schwietz.axel@gmx.de>
2022-06-21 11:03:14 +0200

    first draft

--- exercise_day_2_axel.py
+++ exercise_day_2_axel.py
@@ -1,1 +1,19 @@
 #
+vari="field0"
+varj="field1"
+vark="field2"
+varl="field3"
+
+myList = []
+myList.append(vari)
+myList.append(varj)
+myList += [vark]
+
+
+print(myList)
+print("\n ------- \n")
+for i in myList:
+    print(i)
+
+
+print("\n ------- \n")
