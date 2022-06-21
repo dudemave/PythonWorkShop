bdbf9ab1c2ce0a5d3524dd2f37bf4c0d755a1ea1
dudemave <marekheinrich@web.de>
2022-06-21 14:04:48 +0200

    some more machine infrastructure - wip

--- franco_machine/machine.py
+++ franco_machine/machine.py
@@ -1,1 +1,21 @@
 #!/usr/bin/env python3
+
+
+
+class machine():
+
+
+    def __init__(self):
+
+        self.machine_type=None
+        self.country=None
+        self.sw_version=None
+
+        self.rate_table=[]
+        self.rt_version=None
+
+        self.postal_sn=None
+
+        self.haben = None
+        self.verbrauch = None
+        self.total_register = None
