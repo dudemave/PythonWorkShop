9d7e6bc61941df1c632670fd5c00a526f94238b0
123wall <123wallroth321@posteo.de>
2022-06-22 10:57:06 +0200

    MyClass1 Übung Andreas

---
+++ MyClass1.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# MyClass1
+
+class MyClass1():
+    def __init__ (self): #, value1, value2, text1, result=0 ):
+        pass
+
+
+    def MyFunction(self, value1, value2, text1):
+        print()
+        print(text1, value1, text1, value2)
+
+newObj = MyClass1()
+
+## Aufruf
+newObj.MyFunction(value1=200, value2=330, text1='value: ')
