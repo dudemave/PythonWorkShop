01df1172d7f6b4606f3e9eb4482050fd037e70c4
dudemave <marekheinrich@web.de>
2022-06-22 11:14:20 +0200

    info

--- lego/helloWorld.py
+++ lego/helloWorld.py
@@ -1,3 +1,6 @@
 from ev3dev2.sound import Sound
+from ev3dev2.sensor.lego import TouchSensor, UltraSonicSensor
+
+from ev3dev2.sensor import INPUT_1, INPUT_2
 sound = Sound()
 sound.speak('hello world')
