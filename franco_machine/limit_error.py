8444375da9bd5d69c60c16ab46221d7c6d8160fa
dudemave <marekheinrich@web.de>
2022-06-22 14:01:10 +0200

    cleaning up - removing junk

--- franco_machine/limit_error.py
+++ franco_machine/limit_error.py
@@ -1,20 +1,4 @@
 
+
 class LimitError(BaseException):
     pass
-
-
-
-def very_dangerous_function():
-    """evil things happen here, easily breaks your code"""
-    ### ToDo: add crazy stuff here
-
-    raise LimitError("es klappt leider nicht")
-
-
-
-
-try:
-    very_dangerous_function()
-
-except LimitError:
-    print("hurra es klappt")
