dc3375e5c8b908adfda7c168b0a709cbc5f81828
Axel Schwietz <schwietz.axel@gmx.de>
2022-06-23 13:49:26 +0200

    hfuifh

---
+++ exercise_day_4_axel.py
@@ -0,0 +1,42 @@
+#
+print()
+print()
+print("\t demonstration dictionaries and random")
+import random
+print()
+print()
+
+#random.seed(2)
+#all_product_names = myMachine.rate_table.rates.keys()
+all_prd = {
+    "Standard Letter 100gr" :  0.85 ,
+    "Einschreiben 50 gr"    :  1.80 ,
+    "xyz 150 gr"   :  2.70 ,
+    "abc 500 gr"   :  3.50 ,
+    }
+
+print(all_prd)
+#print(all_prd_names.__dict__)
+# xy = myMachine.rate_table.rates.values()
+print(list(all_prd))
+print(random.choice(list(all_prd)))
+print(random.choice(list(all_prd)))
+print(random.choice(list(all_prd)))
+print(random.choice(list(all_prd)))
+print(random.choice(list(all_prd)))
+print(random.choice(list(all_prd)))
+# 4 x wir ein zufäliger Wert des Dictionaries augegeben
+
+print()
+print("Items:")
+for vv in all_prd.items():
+    print(vv)
+print()
+print("Values:")
+for vv in all_prd.values():
+    print(vv)
+print()
+print("Keys:")
+for vv in all_prd.keys():
+    print(vv)
+print()
